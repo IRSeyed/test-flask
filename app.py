7788e516def8df9bcaba29aeab46a2ff8a9a0f62
IRSeyed <83493055+IRSeyed@users.noreply.github.com>
2022-09-03 00:40:54 +0430

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 @app.route('/test')
 def test():
-    return 'test' 
+    file = open('data.json', 'w')
+    return jsonify(json.load(file))
+
+@app.route('/getreq')
+def get_req():
+    variable = request.get_json()
+    file = open('data.json', 'w')
+    json.dump(variable, file)
+    file.close()
+    return 'ok'
+
